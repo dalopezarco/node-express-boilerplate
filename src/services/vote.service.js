18ca38c04a4d9a503e06949ccf4828124d82a76d
Daniel Lopez <danyjaen90@gmail.com>
2021-05-09 17:37:51 +0200

    votes management

--- src/services/vote.service.js
+++ src/services/vote.service.js
@@ -9,15 +9,31 @@ const vote = async (voteBody, user) => {
   if (!product) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Product doesnt exist');
   }
-  if (user.votedOn.includes(product.id)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Already voted on this product');
-  }
+  const votedIndex = user.votedOn.findIndex((v) => v.productId == product.id);
   const userUpdated = user;
-  userUpdated.votedOn.push({ productId: product.id, type: voteBody.type });
-  if (voteBody.type === 'up') {
-    product.votes += 1;
-  } else if (voteBody.type === 'down') {
-    product.votes -= 1;
+  if (votedIndex !== -1) {
+    if (voteBody.type === 'up' && user.votedOn[votedIndex].type === 'up') {
+      product.votes -= 1;
+      userUpdated.votedOn.splice(votedIndex, 1);
+    } else if (voteBody.type === 'up' && user.votedOn[votedIndex].type === 'down') {
+      product.votes += 2;
+      userUpdated.votedOn.splice(votedIndex, 1);
+      userUpdated.votedOn.push({ productId: product.id, type: voteBody.type });
+    } else if (voteBody.type === 'down' && user.votedOn[votedIndex].type === 'down') {
+      product.votes += 1;
+      userUpdated.votedOn.splice(votedIndex, 1);
+    } else if (voteBody.type === 'down' && user.votedOn[votedIndex].type === 'up') {
+      product.votes -= 2;
+      userUpdated.votedOn.splice(votedIndex, 1);
+      userUpdated.votedOn.push({ productId: product.id, type: voteBody.type });
+    }
+  } else {
+    userUpdated.votedOn.push({ productId: product.id, type: voteBody.type });
+    if (voteBody.type === 'up') {
+      product.votes += 1;
+    } else if (voteBody.type === 'down') {
+      product.votes -= 1;
+    }
   }
   await userService.updateUserById(user.id, userUpdated);
   await productService.updateProductById(voteBody.productId, product);
